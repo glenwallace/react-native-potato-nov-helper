9d7dc7e72e7777a982379d4515400156d37aee64
zack <117136075+glenwallace@users.noreply.github.com>
2023-11-15 14:04:56 +0800

    update

---
+++ ios/AppDelegate+Nov.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+Nov.h
+//  SlidingWizard
+//
+//  Created by zack on 11/15/23.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppDelegate (Nov)
+
+@end
+
+NS_ASSUME_NONNULL_END
